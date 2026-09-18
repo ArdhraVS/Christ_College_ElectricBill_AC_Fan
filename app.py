42d3d3345e186566ba2049d19911a32ebb4f08f1
ArdhraVS <ardhra139@gmail.com>
2026-09-18 11:09:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 from sklearn.preprocessing import PolynomialFeatures
 
 model=joblib.load("PolynomialRegression_ElectricBill_AC_Fan.pkl")
-st.title("Electricity Bill Prediction Based On AC Units")
+st.title("Electricity Bill Prediction Based On AC Units and Fan Units")
 
 ac_units=st.number_input("Enter AC Units : ", min_value=0.0, value=100.0)
 fan_units=st.number_input("Enter Fan Units : ", min_value=0.0, value=100.0)
